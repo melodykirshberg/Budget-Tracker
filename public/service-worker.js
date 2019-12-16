b350a7f0fd878d066ac32199b4a2fea129f5dd5f
verlitas <55808608+verlitas@users.noreply.github.com>
2019-12-16 14:43:32 -0700

    took out comments

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,6 @@ const FILES_TO_CACHE = ["/", "index.html", "index.js", "service-worker.js", "man
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// install
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -15,7 +14,6 @@ self.addEventListener("install", function (evt) {
     self.skipWaiting();
 });
 
-// activate
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
